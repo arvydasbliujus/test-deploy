380b35f93de1cb3023726d9b5686665a5e2bd1fb
Arvydas <arvydasbliujus@gmail.com>
2020-07-02 14:14:48 +0300

    Updates

---
+++ precache-manifest.19792c3e3641893b3b47939b18cbd667.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d526a3e9d0f3d1cd1fb52af7adce156b",
+    "url": "/test-deploy/index.html"
+  },
+  {
+    "revision": "13a37f7d865914919546",
+    "url": "/test-deploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c589fc5603146bac3a7e",
+    "url": "/test-deploy/static/js/2.b729121a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-deploy/static/js/2.b729121a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13a37f7d865914919546",
+    "url": "/test-deploy/static/js/main.5d7acbcb.chunk.js"
+  },
+  {
+    "revision": "744b94d0a3bd4943c88a",
+    "url": "/test-deploy/static/js/runtime-main.37e6c0e5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
